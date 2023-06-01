d2ee7ce3858075cb15418f25f43d56750ddb6ce1
Marcel Scheck <marcel.scheck@hetzner-cloud.de>
2023-06-01 15:23:18 +0200

    WIP

---
+++ setup.sh
@@ -0,0 +1,24 @@
+# install homebrew
+echo "checking brew installation"
+if brew -v
+then
+    echo "brew already installed - skipping installation"
+else
+    echo "installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# update brew
+brew update
+
+# install iterm2 + zsh
+brew install --cask iterm2
+brew install zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+brew install git
+
+
+
